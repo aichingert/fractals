590929fb276ad8fdc1d04538f4dd2d3f929cbb0b
TobiasAichinger <aichinger.tobias13@gmail.com>
2022-08-17 21:24:15 +0200

    removed files outside the folder

--- README.md
+++
@@ -1,4 +0,0 @@
-This is my first time using vulkan
-
-This project is following the tutorial from: Brendan Galea
-(Youtube: https://www.youtube.com/c/BrendanGalea/videos)
--- src/app.cpp
+++
@@ -1,9 +0,0 @@
-#include "app.hpp"
-
-namespace srp {
-    void App::run() {
-        while (!srpWindow.shouldClose()) {
-            glfwPollEvents();
-        }
-    }
-}
